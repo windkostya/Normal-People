7a76c0680a1f57468a4657189e49229721e2eb14
windkostya <kostyayaremtso@ebiosddlsrmach1.affymetrix.com>
2016-11-18 11:41:30 -0800

    Initial Commit

---
+++ Normal People/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Normal People
+//
+//  Created by Kostya Yaremtso on 11/18/16.
+//  Copyright © 2016 Kostya Yaremtso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
